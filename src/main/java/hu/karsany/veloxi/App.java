76f0f7882d46258147fbc058dd0569b6a7a85c9d
Ferenc Karsany <karsany@karsany.hu>
2013-01-04 01:41:38 +0100

    work without absolute velocity path

--- src/main/java/hu/karsany/veloxi/App.java
+++ src/main/java/hu/karsany/veloxi/App.java
@@ -17,7 +17,7 @@ public class App {
         XMLParser parser = new XMLParser(xmlFile);
 
         Map<String, Object> map = parser.parse();
-        VelocityOutput output = new VelocityOutput(new File(args[1]));
+        VelocityOutput output = new VelocityOutput(new File(new File(args[1]).getAbsolutePath()));
 
 
         // new MapOutput(map).print();
